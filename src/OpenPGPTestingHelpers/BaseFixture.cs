abdc0c71e9ac2ad7dbd12246c906d07681c78e81
Bryan Slatner <bryan@slatner.com>
2011-01-06 23:37:35 -0500

    Added base class for test fixtures

---
+++ src/OpenPGPTestingHelpers/BaseFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace OpenPGPTestingHelpers
+{
+    public class BaseFixture
+    {
+        protected static Stream GetTestDataAsStream(string name)
+        {
+            var asm = Assembly.GetCallingAssembly();
+            var resourceName = string.Format("{0}.TestData.{1}", asm.GetName().Name, name);
+            var stream = ResourceHelper.GetResourceAsStream(asm, resourceName);
+            if (stream == null)
+            {
+                throw new InvalidOperationException(string.Format("Could not find {0}", resourceName));
+            }
+            return stream;
+        }
+    }
+}
