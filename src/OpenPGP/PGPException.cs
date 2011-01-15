57b9fc1fcb47592b7c3ef790aa408e50b703328f
Bryan Slatner <bryan@slatner.com>
2011-01-14 22:56:32 -0500

    Added base exception type

---
+++ src/OpenPGP/PGPException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace OpenPGP
+{
+    [Serializable]
+    public class PGPException : Exception
+    {
+        public PGPException()
+        {
+        }
+
+        public PGPException(string message) : base(message)
+        {
+        }
+
+        public PGPException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected PGPException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
