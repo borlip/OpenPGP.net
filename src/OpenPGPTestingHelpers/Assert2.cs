1204b2293e75bfade1bbc620d3d2baf94d5de6fd
Bryan Slatner <bryan@slatner.com>
2011-01-21 08:07:04 -0500

    Added method for asserting against the first N elements of a sequence

--- src/OpenPGPTestingHelpers/Assert2.cs
+++ src/OpenPGPTestingHelpers/Assert2.cs
@@ -10,17 +10,39 @@ namespace OpenPGPTestingHelpers
         {
             var enumExpected = expected.GetEnumerator();
             var enumValue = value.GetEnumerator();
+            var position = 0;
 
             while (true)
             {
                 var haveNextExpected = enumExpected.MoveNext();
                 var haveNextValue = enumValue.MoveNext();
-                Assert.AreEqual(haveNextExpected, haveNextValue);
+                Assert.AreEqual(haveNextExpected, haveNextValue, "Number of elements differs");
                 if (!haveNextExpected)
                 {
                     break;
                 }
-                Assert.AreEqual(enumExpected.Current, enumValue.Current);
+                Assert.AreEqual(enumExpected.Current, enumValue.Current, "Element at position {0} differs", position);
+                position++;
+            }
+        }
+
+        public static void AreElementsEqual<T>(IEnumerable<T> expected, IEnumerable<T> value, int count) where T : IComparable
+        {
+            var enumExpected = expected.GetEnumerator();
+            var enumValue = value.GetEnumerator();
+            var position = 0;
+
+            while (position < count)
+            {
+                var haveNextExpected = enumExpected.MoveNext();
+                var haveNextValue = enumValue.MoveNext();
+                Assert.AreEqual(haveNextExpected, haveNextValue, "Insufficient elements to compare");
+                if (!haveNextExpected)
+                {
+                    break;
+                }
+                Assert.AreEqual(enumExpected.Current, enumValue.Current, "Element at position {0} differs", position);
+                position++;
             }
         }
 
